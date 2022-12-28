775c0f82054fe86d7c0507408fe02a3a057c7050
sayak <sayak357@gmail.com>
2022-12-28 12:30:46 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
 
   const [coins, setCoins] = useState([])
 
-  const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=200&page=1&sparkline=false'
+  const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=150&page=1&sparkline=false'
 
   useEffect(() => {
     axios.get(url).then((response) => {
